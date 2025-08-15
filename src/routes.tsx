99e82ba3b3f7323efc916c6fce0429c8987fd841
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-08-14 21:10:58 -0300

    ok

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,41 +1,38 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 
 import { Home } from './pages/Home'
-
 import { Perfil } from './pages/Perfil'
 import { Detalhes } from './pages/Detalhes'
 import { Checkout } from './components/Checkout'
 
-
-
 export const Rotas = () => {
-    const location = useLocation()
-  // Se veio de uma rota anterior, salva ela como background
+  const location = useLocation()
+  const state = location.state as { backgroundLocation?: Location }
 
-    const state = location.state as { backgroundLocation?: Location }
+  return (
+    <>
+      <Routes location={state?.backgroundLocation || location}>
+        <Route path='/' element={<Home />} />
+        <Route path='/restaurante/:id' element={<Perfil />} />
+        <Route
+          path='/checkout'
+          element={
+            <Checkout
+              voltar={() => { throw new Error('Function not implemented.') }}
+              carregarPagamento={() => { throw new Error('Function not implemented.') }}
+              produtos={[]}
+              setEntrega={() => { throw new Error('Function not implemented.') }}
+            />
+          }
+        />
+      </Routes>
 
-    return(
-        <>
-            <Routes location={state?.backgroundLocation || location}>
-                <Route path='/' element={<Home/>} />
-                <Route path='/restaurante/:id' element={<Perfil />} />
-                <Route path="/checkout" element={<Checkout voltar={function (): void {
-                    throw new Error('Function not implemented.')
-                } } carregarPagamento={function (): void {
-                    throw new Error('Function not implemented.')
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                } } produtos={[]} setEntrega={function (dados: { quemRecebe: string; endereco: string; cidade: string; CEP: string; numero: string; complemento: string }): void {
-                    throw new Error('Function not implemented.')
-                } } />} />
-            </Routes>
-             {/* Modal separado */}
-            {state?.backgroundLocation && (
-            <Routes>
-                <Route path="/restaurante/:restauranteId/prato/:id" element={<Detalhes />} />
-            </Routes>
-            )}
-        </>
-    )
+      {/* Modal separado */}
+      {state?.backgroundLocation && (
+        <Routes>
+          <Route path='/restaurante/:restauranteId/prato/:id' element={<Detalhes />} />
+        </Routes>
+      )}
+    </>
+  )
 }
-
-// path="/perfil/:perfilId/restaurante/:restauranteId/prato/:id" element={<Detalhes />}
